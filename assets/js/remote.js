080786922961784b79516a90f7a1d994671f7431
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-08-28 13:41:47 -0700

    dynamically require SocketPeer library from `remote.js`

--- assets/js/remote.js
+++ assets/js/remote.js
@@ -1,22 +1,55 @@
 /* global ga, SocketPeer */
 (function () {
   var rootPath = '/';
-  var socketPath = 'https://remote.webvr.rocks/socketpeer/';
+  // var remoteSocketPath = 'https://remote.webvr.rocks/socketpeer/';
+  var remoteSocketPath = 'http://0.0.0.0:3000/socketpeer/';
 
-  var sceneEl = document.querySelector('#scene');
-  if (sceneEl) {
-    sceneEl.setAttribute('data-remote-state', 'pending');
+  var remoteEl = document.querySelector('#remote');
+  if (remoteEl) {
+    remoteEl.setAttribute('data-remote-state', 'pending');
   }
+
+  function Dependencies () {
+    this.dependencies = {};
+  }
+  Dependencies.prototype.require = function (src, cb) {
+    var self = this;
+    if (self.hasLoaded(src)) {
+      return self.dependencies[src];
+    }
+
+    var script = document.createElement('script');
+    script.src = src;
+    script.async = script.defer = true;
+
+    self.dependencies[src] = null;
+
+    script.addEventListener('load', function () {
+      self.dependencies[src] = true;
+    });
+
+    script.addEventListener('error', function (err) {
+      self.dependencies[src] = err;
+    });
+
+    document.head.appendChild(script);
+  };
+  Dependencies.prototype.hasLoaded = function (src) {
+    return src in this.dependencies;
+  };
+
+  var dependencies = new Dependencies();
 
   window.addEventListener('load', function () {
     rootPath = document.documentElement.getAttribute('data-root') || rootPath;
     remoteSocketPath = document.documentElement.getAttribute('data-remote-socket-path') || remoteSocketPath;
 
-    sceneEl = document.querySelector('#scene');
+    remoteEl = document.querySelector('#remote');
+
+    remoteEl.setAttribute('data-remote-state', 'pending');
 
     setTimeout(function () {
-    sceneEl.setAttribute('data-remote-state', 'loaded');
-      sceneEl.setAttribute('data-remote-state', 'loaded');
+      remoteEl.setAttribute('data-remote-state', 'loaded');
     });
 
     function newPin () {
@@ -41,25 +74,38 @@
     var pathname = window.location.pathname;
     var pathQS = window.location.search;
     var pathHash = window.location.hash;
-
     var code = '';
-    if (/^\/[0-9]+$/.test(pathname)) {
-      code = pathname.substr(1);
-    } else if (/^\?[0-9]+$/.test(pathQS)) {
-      code = pathQS.substr(1);
-      // Change `?1234` to `/1234`.
-      code = redirectToNewPin();
-      peerConnect(code);
-    } else if (/^#[0-9]+$/.test(pathHash)) {
-      code = pathHash.substr(1);
-      // Change `#1234` to `/1234`.
-      code = redirectToNewPin();
-      peerConnect(code);
+
+    function handleCurrentPin (code) {
+      pathname = window.location.pathname;
+      pathQS = window.location.search;
+      pathHash = window.location.hash;
+
+      if (!code) {
+        code = '';
+        if (/^\/[0-9]+$/.test(pathname)) {
+          code = pathname.substr(1);
+        } else if (/^\?[0-9]+$/.test(pathQS)) {
+          code = pathQS.substr(1);
+          code = redirectToNewPin(code);  // Change `?1234` to `/1234`.
+        } else if (/^#[0-9]+$/.test(pathHash)) {
+          code = pathHash.substr(1);
+          code = redirectToNewPin(code);  // Change `#1234` to `/1234`.
+        }
+      }
+
+      if (code) {
+        return peerConnect(code);
+      }
+
+      return null;
     }
 
-    if (code) {
-      peerConnect(code);
-    }
+    window.addEventListener('hashchange', function () {
+      handleCurrentPin();
+    });
+
+    handleCurrentPin();
 
     var peerConnectBtnEl = document.querySelector('#peer-connect-btn');
     if (peerConnectBtnEl) {
@@ -70,62 +116,79 @@
       });
     }
 
-    function peerConnect (code) {
-      if (!code) {
-        throw 'PIN code required to connect to remote-control service';
+    function peerConnect (code, cb) {
+      return dependencies.require(remoteSocketPath + 'socketpeer.js', function () {
+        _peerConnect(cb);
+      });
+
+      function _peerConnect (cb) {
+        if (!code) {
+          var err = new Error('PIN code required');
+          if (cb) {
+            return cb(err);
+          } else {
+            throw err;
+          }
+        }
+
+        var peer = new SocketPeer({
+          pairCode: code,
+          url: remoteSocketPath
+        });
+        var connected = false;
+
+        function directionChanged (value) {
+          log('<button press> ' + value);
+
+          peer.send(value);
+
+          var stateData = {
+            type: 'buttondown',
+            value: value
+          };
+
+          window.top.postMessage(JSON.stringify(stateData), '*');
+
+          window.location.hash = '#' + value;
+
+          return stateData;
+        }
+
+        peer.on('data', function (data) {
+          window.top.postMessage(data, '*');
+          log('<them> ' + data);
+        });
+
+        peer.on('upgrade', function () {
+          log('upgraded to p2p');
+          connected = true;
+        });
+
+        peer.on('upgrade_attempt', function () {
+          log('negotiating with peer');
+        });
+
+        peer.on('busy', function () {
+          log('peer connected to someone else');
+        });
+
+        peer.on('error', function (err) {
+          log('connection error: ' + err);
+        });
+
+        peer.on('downgrade', function () {
+          log('p2p connection broken');
+          connected = false;
+        });
+
+        log('waiting for another user to go to ' + location.href);
+
+        if (cb) {
+          return cb(null, peer);
+        } else {
+          return peer;
+        }
       }
-
-      var peer = new SocketPeer({
-        pairCode: code,
-        url: remoteSocketPath
-      });
-      var connected = false;
-
-      function directionChanged (value) {
-        log('<button press> ' + value);
-
-        peer.send(value);
-
-        var stateData = {
-          type: 'buttondown',
-          value: value
-        };
-
-        window.top.postMessage(JSON.stringify(stateData), '*');
-
-        window.location.hash = '#' + value;
-
-        return stateData;
-      }
-
-      peer.on('data', function (data) {
-        window.top.postMessage(data, '*');
-        log('<them> ' + data);
-      });
-
-      peer.on('upgrade', function () {
-        log('upgraded to p2p');
-        connected = true;
-      });
-
-      peer.on('upgrade_attempt', function () {
-        log('negotiating with peer');
-      });
-
-      peer.on('busy', function () {
-        log('peer connected to someone else');
-      });
-
-      peer.on('error', function (err) {
-        log('connection error: ' + err);
-      });
-
-      peer.on('downgrade', function () {
-        log('p2p connection broken');
-        connected = false;
-      });
-
-      log('waiting for another user to go to ' + location.href);
     }
   });
 })();
