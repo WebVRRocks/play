e1639c443a71eea26abca9f61c02b58e9bfe9656
Christopher Van Wiemeersch <hearcomestreble@gmail.com>
2017-08-29 14:23:21 -0700

    add code for detecting whether `remote` is running locally and rewrite the URL defined in the `data-remote-socket-path` attribute in HTML responses

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,17 +1,29 @@
+const http = require('http');
+const https = require('https');
 const urlParse = require('url').parse;
 
 const liveServer = require('live-server');
 
 const spaRoutes = require('./routes.json');
 
+const remoteHost = process.env.REMOTE_HOST || '0.0.0.0';
+const remotePort = parseInt(process.env.REMOTE_PORT || '3000', 10);
+const remoteHttps = process.env.REMOTE_HTTPS === '1' || process.env.REMOTE_HTTPS === 'true';
+
+const remoteSocketPathname = '/socketpeer/';
+const remoteSocketPath = `http${remoteHttps ? 's' : ''}://${remoteHost}:${remotePort}${remoteSocketPathname}`;
+const prodRemoteSocketPath = 'https://remote.webvr.rocks/socketpeer/';
+
 let settings = {
   root: process.env.WWW_ROOT || process.env.ROOT || process.cwd(),
-  port: process.env.WWW_PORT || process.env.PORT || 8080,
   host: process.env.WWW_HOST || process.env.HOST || '0.0.0.0',
+  port: process.env.WWW_PORT || process.env.PORT || 8080,
   open: (process.env.WWW_OPEN || process.env.OPEN) === 'true',
   ignore: process.env.WWW_IGNORE || process.env.IGNORE || '.git,node_modules',
   logLevel: parseInt(process.env.WWW_LOGS || process.env.LOGS || '2', 10),  // 0 = errors only; 1 = some errors; 2 = many errors.
-  middleware: []
+  middleware: [
+    redirectMiddleware
+  ]
 };
 
 function redirectMiddleware (req, res, next) {
@@ -33,6 +45,56 @@ function redirectMiddleware (req, res, next) {
   next();
 }
 
-settings.middleware.push(redirectMiddleware);
+const rewriteRemoteSocketPathMiddleware = require('connect-body-rewrite')({
+  accept: function (res) {
+    return res.getHeader('content-type').match(/text\/html/i);
+  },
+  rewrite: function (body) {
+    return body.replace(/data-remote-socket-path=.*/, `data-remote-socket-path="${remoteSocketPath}"`);
+  }
+});
 
-liveServer.start(settings);
+function startServer (socketpeerRunningLocally) {
+  if (socketpeerRunningLocally) {
+    console.log(`Using detected local Remote Control server: ${remoteSocketPath}`);
+    settings.middleware.push(rewriteRemoteSocketPathMiddleware);
+  } else {
+    console.log(`Falling back to hosted Remote Control server: ${prodRemoteSocketPath}`);
+  }
+  liveServer.start(settings);
+}
+
+let socketpeerRunningLocally = false;
+let protocolHandler = remoteHttps ? https : http;
+const spReqOptions = {
+  method: 'GET',
+  protocol: `http${remoteHttps ? 's' : ''}:`,
+  hostname: remoteHost,
+  port: remotePort,
+  path: remoteSocketPathname + 'socketpeer.js'
+};
+new Promise((resolve, reject) => {
+  let reqTimeout = setTimeout(() => {
+    reject(new Error('Request timed out after 3 seconds'));
+  }, 3000);
+  let req = http.request(spReqOptions, res => {
+    res.setEncoding('utf8');
+    res.on('error', () => {
+      reject('Error occurred');
+    });
+    res.on('end', () => {
+      clearTimeout(reqTimeout);
+    });
+    resolve(res.statusCode);
+  });
+  req.on('error', err => {
+    reject(err);
+  });
+  req.end();
+}).then(statusCode => {
+  const socketpeerRunningLocally = statusCode === 200;
+  startServer(socketpeerRunningLocally);
+}).catch(err => {
+  console.warn(`Could not reach Remote Control server: ${remoteSocketPath}`);
+  startServer(false);
+});
